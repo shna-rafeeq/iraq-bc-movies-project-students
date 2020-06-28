a2fa1a197e2b5a9218e64896c653bb9c149debec
shna rafeeq <shnar.majeed@gmail.com>
2020-06-29 01:47:13 +0300

    v3-responsive

--- bawan-shna-abdulrahman/src/Components/MovieGrid.js
+++ bawan-shna-abdulrahman/src/Components/MovieGrid.js
@@ -1,27 +1,23 @@
 import React from "react";
 import RenderMovie from "./RenderMovie";
-import { Row, Col } from "react-simple-flex-grid";
+import { Row } from "react-simple-flex-grid";
+import { Container } from "react-bootstrap";
 
 export default function MoviesGrid(props) {
   const movies = props.movies;
   return (
-    <>
-      <Row gutter={40}>
+    <Container>
+      <Row className="justify-content-center">
         {movies.length > 0 &&
           movies.map((movie) => (
-            <Col span={3} align="middle">
-              <RenderMovie
-                overview={movie.overview}
-                src={
-                  "https://image.tmdb.org/t/p/original" + movie.backdrop_path
-                }
-                key={movie.id}
-                title={movie.title}
-              />
-            </Col>
+            <RenderMovie
+              overview={movie.overview}
+              src={"https://image.tmdb.org/t/p/original" + movie.backdrop_path}
+              key={movie.id}
+              title={movie.title}
+            />
           ))}
       </Row>
-      {/* <RenderMovie movies={props.movies} /> */}
-    </>
+    </Container>
   );
 }
--- bawan-shna-abdulrahman/src/Components/RenderMovie.js
+++ bawan-shna-abdulrahman/src/Components/RenderMovie.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Card, CardTitle, CardText } from "react-bootstrap";
 
 export default function RenderMovie(props) {
   const [show, setShow] = useState(false);
@@ -8,37 +8,35 @@ export default function RenderMovie(props) {
   const handleShow = () => setShow(true);
 
   return (
-    <div style={{ marginTop: "50px", marginBottom: "50px" }}>
-      <p>{props.title}</p>
-      <img
-        src={props.src}
-        alt="movie image"
-        width="380"
-        height="300"
-        style={{ borderRadius: "3px" }}
-      />
+    <div style={{ marginTop: "50px" }}>
+      <Card style={{ width: "300px", margin: "10px" }}>
+        <Card.Img variant="top" src={props.src} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
 
-      <Button
-        variant="warning"
-        onClick={handleShow}
-        style={{ marginTop: "10px" }}
-      >
-        The Overview
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>OverView</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>{props.overview}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="warning" onClick={handleClose}>
-            Close
+          <Button
+            variant="warning"
+            onClick={handleShow}
+            style={{ marginTop: "10px" }}
+          >
+            Overview
           </Button>
-        </Modal.Footer>
-      </Modal>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>OverView</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>{props.overview}</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="warning" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
