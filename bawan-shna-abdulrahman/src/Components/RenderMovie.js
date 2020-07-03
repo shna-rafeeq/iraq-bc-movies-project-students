cb5db35c258a82da093e8b19e7ef7f682fd56fc5
shna rafeeq <shnar.majeed@gmail.com>
2020-07-03 19:37:20 +0300

    createModal

--- bawan-shna-abdulrahman/src/Components/RenderMovie.js
+++ bawan-shna-abdulrahman/src/Components/RenderMovie.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Badge } from "react-bootstrap";
+import { Card, Badge, Modal, Button, toogle } from "react-bootstrap";
 import "./BoxShadow.css";
 import { Link } from "react-router-dom";
 
@@ -33,6 +33,28 @@ export default function RenderMovie(props) {
               Popularity:{props.popularity}
             </Badge>
             <Badge variant="primary">{props.release_date}</Badge>
+
+            <Button
+              variant="warning"
+              onClick={toogle}
+              style={{ marginTop: "10px" }}
+            >
+              Overview
+            </Button>
+
+            <Modal onHide={toogle}>
+              <Modal.Header closeButton>
+                <Modal.Title>OverView</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p>{props.overview}</p>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="warning" onClick={toogle}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Card.Body>
         </Card>
       </div>
