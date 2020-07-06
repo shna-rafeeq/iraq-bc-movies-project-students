61f1ca5aeef0295502e276b08bd5927d41c8e31a
shna rafeeq <shnar.majeed@gmail.com>
2020-07-06 10:47:17 +0300

    fixingHome

--- bawan-shna-abdulrahman/src/Components/Header.js
+++ bawan-shna-abdulrahman/src/Components/Header.js
@@ -1,17 +1,31 @@
 import React, { useContext } from "react";
-
-import { Navbar, Nav } from "react-bootstrap";
+import { StateContext } from "./StateProvider";
+import { useHistory } from "react-router-dom";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import Search from "./Search";
 import { Link } from "react-router-dom";
+import { constructUrl } from "./Api";
 import { faFilm } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
+  const [state, dispatch] = useContext(StateContext);
+  const history = useHistory();
+  const test = () => {
+    let SEARCH_URL;
+    SEARCH_URL = constructUrl("movie/popular");
+    fetch(SEARCH_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "SET_MOVIES", payload: data.results });
+      });
+    history.push("/");
+  };
   return (
     <div>
       <Navbar bg="dark" expand="lg" style={{ zIndex: 1000 }}>
-        <Nav.Link
-          href="/"
+        <Link
+          to="/"
           style={{
             marginRight: "20px",
             color: "white",
@@ -22,13 +36,16 @@ export default function Header() {
             icon={faFilm}
             style={{ fontSize: "25px", color: "#FFC107" }}
           />
-        </Nav.Link>
+        </Link>
         <Navbar.Toggle aria-controls="basic-Navbar-nav" />
         <Navbar.Collapse id="basic-Navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/" style={{ color: "white" }}>
+            {/* <Link to="/" style={{ color: "white" }}>
               Home
-            </Nav.Link>
+            </Link> */}
+            <Button variant="dark" onClick={test} className="home-btn">
+              Home
+            </Button>
           </Nav>
           <Search />
         </Navbar.Collapse>
